860ebb4d15aba6321b9de5dd83ca8ebda583bca3
jes3rk <joseph.schefer@gmail.com>
2020-07-03 19:46:18 -0400

    set up testing and env amangement

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,14 @@
 import express from 'express';
-
+import { environ } from './utils';
 export const app = express();
+
+/**
+ * Middleware to enable some logging
+ */
+app.use((req, res, next) => {
+    if (environ('NODE_ENV', 'no').toUpperCase() !== 'TEST') console.log(`${new Date().toUTCString()}: ${req.method.toUpperCase()} ${req.url} - ${req.ip}`)
+    next()
+})
 
 app.get('/', ( req, res) => {
     res.status(200).send('Hello World')
