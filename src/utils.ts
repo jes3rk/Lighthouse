2cfd2cccb559cbe5250e2d4b1927ada639a042a3
jes3rk <joseph.schefer@gmail.com>
2020-07-03 20:02:21 -0400

    starting to hit on the lights now

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Wrapper function to source environment variables
+ * @param varName Name of the variable to find
+ * @param defaultValue Default value if variable does not exist
+ * @throws {TypeError} Non-string passed to function
+ * @throws {ReferenceError} Variable does not exist and no default passed
+ */
 export const environ = (varName: string, defaultValue: string = null): string => {
     if (typeof varName !== "string") throw TypeError('Invalid type for varName. Should be string');
     let response: string = process.env[varName];
